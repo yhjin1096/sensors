0da789f1ecf6144e8c7ba3739268706dda0080bd
Hyeokjin Yun <yhjin1096@gamil.com>
2024-10-07 17:40:02 +0900

    luxonis oak-1 w

---
+++ src/luxonis/include/luxonis/luxonis.hpp
@@ -0,0 +1,10 @@
+#ifndef luxonis_hpp
+#define luxonis_hpp
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+// Includes common necessary includes for development using depthai library
+#include "depthai/depthai.hpp"
+
+#endif
---
+++ src/luxonis/src/oak-1_w.cpp
@@ -0,0 +1,42 @@
+#include "luxonis/luxonis.hpp"
+
+int main() {
+    // Create pipeline
+    dai::Pipeline pipeline;
+
+    // Define source and output
+    auto camRgb = pipeline.create<dai::node::ColorCamera>();
+    auto xoutVideo = pipeline.create<dai::node::XLinkOut>();
+
+    xoutVideo->setStreamName("video");
+
+    // Properties
+    camRgb->setBoardSocket(dai::CameraBoardSocket::CAM_A);
+    camRgb->setResolution(dai::ColorCameraProperties::SensorResolution::THE_2024X1520);
+    camRgb->setVideoSize(2024, 1520);
+
+    xoutVideo->input.setBlocking(false);
+    xoutVideo->input.setQueueSize(1);
+
+    // Linking
+    camRgb->video.link(xoutVideo->input);
+
+    // Connect to device and start pipeline
+    dai::Device device(pipeline);
+
+    auto video = device.getOutputQueue("video");
+
+    while(true) {
+        auto videoIn = video->get<dai::ImgFrame>();
+
+        // Get BGR frame from NV12 encoded video frame to show with opencv
+        // Visualizing the frame on slower hosts might have overhead
+        cv::imshow("video", videoIn->getCvFrame());
+
+        int key = cv::waitKey(1);
+        if(key == 'q' || key == 'Q') {
+            return 0;
+        }
+    }
+    return 0;
+}
